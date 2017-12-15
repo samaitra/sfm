f2be65b5b9e921a4f930782fefedadfabd25cd4a
samaitra <saikat.maitra@gmail.com>
2017-12-14 23:50:18 -0600

    fixed application runner

--- src/main/kotlin/com/example/sfm/SfmApplication.kt
+++ src/main/kotlin/com/example/sfm/SfmApplication.kt
@@ -7,23 +7,23 @@ import org.springframework.context.annotation.Bean
 import reactor.core.publisher.Flux
 
 @SpringBootApplication
-class SfmApplication
-
-@Bean
-fun runner(pr: ProductRepository) = ApplicationRunner {
-    val products = Flux.just ("Hatchimals", "Rubik Cube", "Lego")
-            .flatMap { pr.save(Product(title = it)) }
-
-    pr.deleteAll()
-            .thenMany(products)
-            .thenMany(pr.findAll())
-            .subscribe( {println(it)})
+class SfmApplication {
+    @Bean
+    fun runner(pr: ProductRepository) = ApplicationRunner {
+        pr.deleteAll()
+                .thenMany(Flux.just("Hatchimals", "Rubik Cube", "Lego")
+                        .flatMap { pr.save(Product(title = it)) })
+                .thenMany(pr.findAll())
+                .subscribe( {println(it)})
+    }
 }
+
 
 
 fun main(args: Array<String>) {
     SpringApplication.run(SfmApplication::class.java, *args)
 }
+
 
 
 
