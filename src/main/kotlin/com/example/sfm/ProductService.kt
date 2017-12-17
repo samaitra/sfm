c235fd5eb481603bb16985d249c9df10877a040f
samaitra <saikat.maitra@gmail.com>
2017-12-16 20:15:20 -0600

    text stream enabled

--- src/main/kotlin/com/example/sfm/ProductService.kt
+++ src/main/kotlin/com/example/sfm/ProductService.kt
@@ -1,6 +1,10 @@
 package com.example.sfm
 
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.SynchronousSink
+import java.time.Duration
+import java.util.*
 
 @Service
 class ProductService (private val pr: ProductRepository) {
@@ -8,4 +12,8 @@ class ProductService (private val pr: ProductRepository) {
     fun all() = pr.findAll()
 
     fun byId(id: String) = pr.findById(id)
+
+    fun events(id: String) = Flux
+    .generate({ sink: SynchronousSink<ProductEvent> -> sink.next(ProductEvent(id, Date())) })
+    .delayElements(Duration.ofSeconds(1L))
 }
--- src/main/kotlin/com/example/sfm/WebConfiguration.kt
+++ src/main/kotlin/com/example/sfm/WebConfiguration.kt
@@ -2,6 +2,7 @@ package com.example.sfm
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
@@ -13,5 +14,6 @@ class WebConfiguration(val ps: ProductService) {
     fun routes() = router {
         GET ("/products", { ServerResponse.ok().body(ps.all(), Product::class.java)})
         GET("/products/{id}", { ServerResponse.ok().body(ps.byId(it.pathVariable("id")), Product::class.java) })
+        GET("/products/{id}/events", { ServerResponse.ok().contentType(MediaType.TEXT_EVENT_STREAM).body(ps.events(it.pathVariable("id")), ProductEvent::class.java) })
     }
 }
