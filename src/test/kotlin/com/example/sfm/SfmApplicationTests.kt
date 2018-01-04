986b915b84f529d136f48d1e67317111ccec3fce
samaitra <saikat.maitra@gmail.com>
2018-01-03 22:31:14 -0600

    updated unit tests and added jackson module in classpath

--- src/test/kotlin/com/example/sfm/SfmApplicationTests.kt
+++ src/test/kotlin/com/example/sfm/SfmApplicationTests.kt
@@ -8,7 +8,9 @@ import org.springframework.context.ApplicationContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.junit.Before
-
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication
+import java.util.function.Consumer
 
 
 @RunWith(SpringRunner::class)
@@ -25,6 +27,7 @@ class SfmApplicationTests {
 		client = WebTestClient
 				.bindToApplicationContext(context)
 				.configureClient()
+				.filter(basicAuthentication())
 				.baseUrl("http://localhost:8080/")
 				.build()
 	}
@@ -36,10 +39,14 @@ class SfmApplicationTests {
 	@Test
 	fun getProducts() {
 		client!!.get().uri("/products/")
+				.attributes(myCredentials())
 				.exchange()
 				.expectStatus()
 				.isOk()
 	}
 
+	private fun myCredentials(): Consumer<Map<String, Any>> {
+		return ExchangeFilterFunctions.Credentials.basicAuthenticationCredentials("admin", "pwd")
+	}
 
 }
